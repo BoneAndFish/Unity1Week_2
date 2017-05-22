5892fd9c59649a28943f2f9923e7422c9b4b3cc5
DESKTOP-K27J26M\o_10s <DeadMansSun@gmail.com>
2017-05-23 01:44:31 +0900

    一週間ゲームジャムのデータを入れておいた。

---
+++ Unity1Week_2/Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceRoll : MonoBehaviour {
+
+    [SerializeField]
+    private Rigidbody rigidBody;
+    public float maxForce;
+
+    public static int diceSurfaceInfo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (rigidBody.velocity.magnitude == 0)
+        {
+            DecideDiceNum();
+        }
+    }
+
+    /// <summary>
+    /// サイコロを転がす.
+    /// </summary>
+    public void RollDice()
+    {
+        Vector3 randomDirection = new Vector3(1f, Random.Range(0f, 1f), 1f);
+        rigidBody.AddForce(randomDirection * Random.Range(maxForce/2,maxForce),ForceMode.Impulse);
+        rigidBody.AddTorque(randomDirection * maxForce*1.15f,ForceMode.Impulse);
+    }
+    /// <summary>
+    /// 値が0のままならもう一度転がす.
+    /// </summary>
+    void DecideDiceNum()
+    {
+        if (diceSurfaceInfo == 0)
+        {
+            Debug.Log("もう一度飛ばすよ");
+            RollDice();
+        }else
+        {
+            Debug.Log("とまったよ");
+        }
+    }
+
+}
---
+++ Unity1Week_2/Assets/Scripts/DiceSurface.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiceSurface : MonoBehaviour {
+
+    [SerializeField]
+    private int thisSurfaceNum;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    /// <summary>
+    /// 一番上の面の値を取得する.
+    /// </summary>
+    void OnTriggerEnter()
+    {
+        int getSurfaceNum = (7 - thisSurfaceNum);
+        Debug.Log("今一番上にあるのはコレ:"+getSurfaceNum);
+        DiceRoll.diceSurfaceInfo = getSurfaceNum;
+    }
+    /// <summary>
+    /// 離れたら0を返して何もとれてないことを伝える.
+    /// </summary>
+    void OnTriggerExit()
+    {
+        DiceRoll.diceSurfaceInfo = 0;
+    }
+}
